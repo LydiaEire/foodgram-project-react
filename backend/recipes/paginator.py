fcfbc26d701fc85a1dd0412bd39e739af30484f4
Your Name <you@example.com>
2021-08-20 18:45:18 +0300

    paginator added

---
+++ backend/recipes/paginator.py
@@ -0,0 +1,16 @@
+from django.core.paginator import EmptyPage, Paginator
+from django.http import Http404
+
+
+class FoodgramPaginator(Paginator):
+    """
+    Prevent 404 error
+    """
+    def validate_number(self, number):
+        try:
+            return super().validate_number(number)
+        except EmptyPage:
+            if number > 1 or number == 0:
+                return self.num_pages
+            else:
+                Http404(f'Page with number {number} was not found')
