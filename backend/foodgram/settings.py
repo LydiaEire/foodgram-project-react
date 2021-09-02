24276e5c089236d923c85bdf82e52406f068bc91
Your Name <you@example.com>
2021-09-03 00:28:22 +0300

    fixing docker settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'bzlb%@*qdr19iu3a1k)248hhqsk_fxg5d+wuhzb6&0-(do4xxp'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '62.84.114.38', 'localhost']
+ALLOWED_HOSTS = ['*', '62.84.113.248', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.auth',
